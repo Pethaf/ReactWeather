28291363a8a8ff1cbb0ff1938c63ec7091c66be8
Pethaf <pethaf1@gmail.com>
2023-12-07 11:24:30 +0100

    Add asyncpaginate to search

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,5 +1,21 @@
-const Search = ():String => {
-    return ("Hello")
+import {  useState } from "react"
+import { OptionsOrGroups, GroupBase } from "react-select"
+import { AsyncPaginate, Response } from "react-select-async-paginate"
+const Search = (onSearchChange:any) => {
+    const [search,setSearch] = useState(null)
+    const handleOnChange = (searchData:any):void => {
+        setSearch(searchData)
+        onSearchChange(searchData)
+    }
+    return (
+    <AsyncPaginate 
+            placeholder="Search For City"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange} loadOptions={function (inputValue: string, options: OptionsOrGroups<null, GroupBase<null>>, additional?: unknown): Response<null, GroupBase<null>, unknown> | Promise<Response<null, GroupBase<null>, unknown>> {
+                throw new Error("Function not implemented.")
+            }} />
+    )
 }
 
 export {Search}
