c82b51b402bc8335d54ea8645532609f8d00d824
Pethaf <pethaf1@gmail.com>
2023-12-11 17:11:22 +0100

    Add support for external coordinates in weather service
    Added support for providing coordinates for request.

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3001; 
 var cors = require('cors')
 app.use(cors())
-
+app.use(express.json());
 app.get("/api/weather",(req,res) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=59.00&lon=18.33&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.long}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
     .then(resp => resp.json())
     .then(res => console.log(res))
     res.send("Hello World")
